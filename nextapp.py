4ffdedfc0fb71aab06899c859c643a49568feeab
AlexFeng <AlexFengCisco>
2016-11-29 22:30:03 +0800

    update topology

--- nextapp.py
+++ nextapp.py
@@ -34,68 +34,41 @@ topologyData01 ={
 };
 
 
-raw_topology={
-  "version": "string",
-  "response": {
+topologyData02={
     "nodes": [
-      {
-        "family": "1",
-        "greyOut": "true",
-        "ip": "string",
-        "userId": "string",
-        "aclApplied": "true",
-        "osType": "string",
-        "id": "1",
-        "roleSource": "string",
-        "label": "1",
-        "upperNode": "2",
-        "deviceType": "router",
-        "platformId": "1",
-        "role": "string",
-        "nodeType": "1",
-        "tags": [
-          "1"
-        ],
-        "vlanId": "string",
-        "networkType": "string",
-        "customParam": {
-          "y": 0,
-          "x": 0,
-          "parentNodeId": "string",
-          "id": "string",
-          "label": "string"
-        },
-        "softwareVersion": "string",
-        "y": 0,
-        "x": 0,
-        "fixed": "true",
-        "dataPathId": "string",
-        "order": 0
-      }
+        {"id": 0, "x": 410, "y": 100, "name": "12K-1"},
+        {"id": 1, "x": 410, "y": 280, "name": "12K-2"},
+        {"id": 2, "x": 660, "y": 280, "name": "Of-9k-03"},
+        {"id": 3, "x": 660, "y": 100, "name": "Of-9k-02"},
+        {"id": 4, "x": 180, "y": 190, "name": "Of-9k-01"}
     ],
-    "id": "1",
     "links": [
-      {
-        "endPortID": "1",
-        "startPortName": "1",
-        "endPortIpv4Address": "1",
-        "startPortID": "string",
-        "greyOut": "true",
-        "endPortName": "1",
-        "endPortIpv4Mask": "string",
-        "id": "1",
-        "source": "1",
-        "tag": "1",
-        "startPortSpeed": "string",
-        "startPortIpv4Address": "string",
-        "endPortSpeed": "string",
-        "linkStatus": "string",
-        "startPortIpv4Mask": "string",
-        "target": "1"
-      }
+        {"source": 0, "target": 1},
+        {"source": 1, "target": 2},
+        {"source": 1, "target": 3},
+        {"source": 4, "target": 1},
+        {"source": 2, "target": 3},
+        {"source": 2, "target": 0},
+        {"source": 0, "target": 4},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3},
+        {"source": 0, "target": 3}
+    ],
+    "nodeSet": [
+        {"id": 5, "type": 'nodeSet', "nodes": [2, 3], "root": '2', "x": 660, "y": 190, "name": "Node set 1", "iconType": 'router'},
+        {"id": 6, "type": 'nodeSet', "nodes": [1, 5], "root": '1', "x": 410, "y": 190, "name": "Node set 2", "iconType": 'groupS'},
+        {"id": 7, "type": 'nodeSet', "nodes": [6, 0], "root": '0', "x": 410, "y": 280, "name": "Node set 3", "iconType": 'groupM'},
+        {"id": 8, "type": 'nodeSet', "nodes": [7, 4], "root": '4', "x": 410, "y": 280, "name": "Node set 4", "iconType": 'groupL'}
     ]
-  }
-}
+};
 
 app = Flask(__name__)
 
@@ -112,7 +85,7 @@ def data():
     text='''Alex_Controller '''
     print text
     node_name="Alex1111"
-    return  jsonify(topologyData01)
+    return  jsonify(topologyData02)
 
 if __name__ == '__main__':
     
